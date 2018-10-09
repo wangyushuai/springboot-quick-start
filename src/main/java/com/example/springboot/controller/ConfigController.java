8dd7019661495e6effb567f9153f0ac614921665
wangyushuai <wangyushuai@fang.com>
2018-10-09 20:34:50 +0800

    添加多环境配置测试Demo

---
+++ src/main/java/com/example/springboot/controller/ConfigController.java
@@ -0,0 +1,27 @@
+package com.example.springboot.controller;
+
+import com.example.springboot.Util.FangResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+/**
+ * Created by wangyushuai@fang.com on 2018/10/9.
+ * 测试多环境配置
+ */
+@RestController
+@RequestMapping("/config")
+@PropertySource("classpath:application.properties")
+public class ConfigController {
+
+    @Value("${server.domain}")
+    private String value;
+
+    @RequestMapping("/get")
+    public Object get() {
+        return FangResponse.buildSuccess(value);
+    }
+}
