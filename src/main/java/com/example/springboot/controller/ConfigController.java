e9eaf981d0029de9036bd3e6cc4c2abcac230057
wangyushuai <wangyushuai@fang.com>
2018-10-11 19:15:26 +0800

    添加多环境配置 实现方式一

--- src/main/java/com/example/springboot/controller/ConfigController.java
+++ src/main/java/com/example/springboot/controller/ConfigController.java
@@ -3,9 +3,12 @@ package com.example.springboot.controller;
 import com.example.springboot.Util.FangResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -13,19 +16,24 @@ import org.springframework.web.bind.annotation.RestController;
  * 测试多环境配置
  */
 @RestController
-@RequestMapping("/config")
 @PropertySource("classpath:application.properties")
 public class ConfigController {
 
     @Value("${server.domain}")
     private String value;
 
+    @Value("${common.config}")
+    private String configValue;
+
     /**
      *  测试多环境配置方法
      * @return
      */
-    @RequestMapping("/get")
+    @GetMapping("config_get")
     public Object get() {
-        return FangResponse.buildSuccess(value);
+        Map<Object,Object>  hashMap = new HashMap<>();
+        hashMap.put("environment",value);
+        hashMap.put("common",configValue);
+        return FangResponse.buildSuccess(hashMap);
     }
 }
