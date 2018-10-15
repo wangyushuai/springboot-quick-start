e94a02bfa4e815129f59cd8d6641558ebfde8bac
wangyushuai <wangyushuai@fang.com>
2018-10-15 11:30:58 +0800

    优化配置文件Demo

--- src/main/java/com/example/springboot/controller/ConfigController.java
+++ src/main/java/com/example/springboot/controller/ConfigController.java
@@ -4,6 +4,7 @@ import com.example.springboot.Util.FangResponse;
 import com.example.springboot.Util.ServerConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,29 +18,34 @@ import java.util.Map;
  * 测试多环境配置
  */
 @RestController
+//@PropertySource("classpath:/config/produce/api.properties")
 public class ConfigController {
 //    defaul.domain = www.fang.com
 //    default.port = 8080
 //    default.local = 127.0.0.1
 
+    @Value("agent.product.detail.domain")
+    private String detailDomain;
+
     @Autowired
     ServerConstant serverConstant;
 
     /**
-     *  测试多环境配置方法
+     *  测试读取 api.properties
      * @return
      */
-//    @GetMapping("/config_app")
-//    public Object get() {
-//        Map<Object,Object>  hashMap = new HashMap<>();
-//        hashMap.put("domain", domain);
-//        hashMap.put("common",configValue);
-//        hashMap.put("evn",evnConfig);
-//        return FangResponse.buildSuccess(hashMap);
-//    }
+    @GetMapping("/config_api_get")
+    public Object get() {
+        Map<Object,Object>  hashMap = new HashMap<>();
+        hashMap.put("detailDomain", detailDomain);
+        return FangResponse.buildSuccess(hashMap);
+    }
 
 
-
+    /**
+     * 测试配置文件自动映射实体集
+     * @return
+     */
     @GetMapping("/config_bean")
     public Map<String,Object> testConfig() {
         Map<String,Object> map = new HashMap<>();
