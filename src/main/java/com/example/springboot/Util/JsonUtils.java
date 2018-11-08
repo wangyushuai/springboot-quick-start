69d976214409661510c65994ba2e09902506c4dd
wangyushuai <wangyushuai@fang.com>
2018-11-08 11:13:35 +0800

    Json 转换工具类

---
+++ src/main/java/com/example/springboot/Util/JsonUtils.java
@@ -0,0 +1,42 @@
+package com.example.springboot.Util;
+
+import java.io.IOException;
+
+import org.springframework.util.StringUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ *  Json字符串转换工具类
+ *  Created by wangyushuai@fang.com on 2018/10/26.
+ */
+public class JsonUtils {
+
+    private static ObjectMapper objectMapper = new ObjectMapper();
+    
+    //对象转字符串
+    public static <T> String obj2String(T obj){
+        if (obj == null){
+            return null;
+        }
+        try {
+            return obj instanceof String ? (String) obj : objectMapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    //字符串转对象
+    public static <T> T string2Obj(String str,Class<T> clazz){
+        if (StringUtils.isEmpty(str) || clazz == null){
+            return null;
+        }
+        try {
+            return clazz.equals(String.class)? (T) str :objectMapper.readValue(str,clazz);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
