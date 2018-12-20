61dac5de9a163a2da55743b683292d9e485e01bf
wangyushuai <wangyushuai@fang.com>
2018-12-20 17:13:39 +0800

    添加发送邮件工具类

---
+++ src/main/java/com/example/springboot/util/email/SendEmail.java
@@ -0,0 +1,13 @@
+package com.example.springboot.util.email;
+
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by wangyushuai@fang.com on 2018/12/10.
+ */
+public class SendEmail {
+
+    public void send() {
+        RestTemplate restTemplate = new RestTemplate();
+    }
+}
